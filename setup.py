de63619630ac5e4bbbe975b1afed7432cff3102f
codeif <me@codeif.com>
2019-12-25 15:08:08 +0800

    可以在win下安装

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python
-import os
-
 from setuptools import find_packages, setup
 
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
+with open('README.rst', encoding='utf-8') as f:
+    readme = f.read()
 
 setup(
     name='flask-http-client',
-    version='0.0.2',
+    version='0.0.4',
     description='HTTP client extension for Flask.',
-    long_description=read('README.rst'),
+    long_description=readme,
     author='codeif',
     author_email='me@codeif.com',
     url='https://github.com/codeif/flask-http-client',
